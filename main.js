f5868ee927b44ccda4fa355c35b95f0b35f8078a
gintssuipe-prog <gints.suipe@gmail.com>
2025-12-18 18:13:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-// ===============================
-// GAME BOOTSTRAP
-// ===============================
-
 const config = {
   type: Phaser.AUTO,
   parent: "game",
-  width: 1100,
-  height: 650,
+  width: 420,
+  height: 820,
   backgroundColor: "#000000",
-
   physics: {
     default: "arcade",
-    arcade: {
-      gravity: { y: 900 },
-      debug: false
-    }
+    arcade: { gravity: { y: 900 }, debug: false }
   },
-
-  scene: [
-    Intro,
-    MainMenu,
-    Stage1
-  ],
-
+  scene: [Stage1],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
 };
 
-// ===============================
-// START GAME
-// ===============================
 new Phaser.Game(config);
