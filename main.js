bbdd46f5a30a518d66d0c97904dd7b99b98bedce
gintssuipe-prog <gints.suipe@gmail.com>
2025-12-19 23:32:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const config = {
   type: Phaser.AUTO,
-  parent: "game",
   width: 420,
   height: 820,
-  backgroundColor: "#000000",
+  parent: "game",
+  scene: [Intro, MainMenu, Stage1],
   physics: {
     default: "arcade",
-    arcade: { gravity: { y: 900 }, debug: false }
-  },
-  scene: [Stage1],
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    arcade: {
+      gravity: { y: 900 },
+      debug: false
+    }
   }
 };
 
