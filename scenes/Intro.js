ce9f383093ae75d40fd8c0ed7b6a0b5ad76e9303
gintssuipe-prog <gints.suipe@gmail.com>
2025-12-21 01:10:24 +0200

    Update Intro.js

--- scenes/Intro.js
+++ scenes/Intro.js
@@ -1,4 +1,4 @@
-// steiblfiksi
+//   kautkas tur bug fix   jo citaadi neiet
 class Intro extends Phaser.Scene {
   constructor() {
     super("Intro");
@@ -6,7 +6,6 @@ class Intro extends Phaser.Scene {
     this._starting = false;
     this._onResize = null;
 
-    // refs cleanup/stabilitātei
     this._bg = null;
     this._grad = null;
     this._hint = null;
@@ -25,16 +24,12 @@ class Intro extends Phaser.Scene {
 
     const isDesktop = !!(this.sys.game.device && this.sys.game.device.os && this.sys.game.device.os.desktop);
 
-    // 1) FONA BILDE (cover)
     const bg = this.add.image(0, 0, "intro_bg").setOrigin(0.5);
     this._bg = bg;
 
-    // 2) Apakšējais gradient overlay
     const grad = this.add.graphics();
     this._grad = grad;
 
-    // 3) © teksts (augšā labajā stūrī)
-    // (pozīcija tiks atjaunota resize laikā)
     const copyright = this.add
       .text(0, 10, "©Gints Suipe", {
         fontFamily: "Arial",
@@ -44,7 +39,6 @@ class Intro extends Phaser.Scene {
       .setOrigin(1, 0)
       .setAlpha(0.9);
 
-    // 4) Hint + START poga
     const hint = this.add
       .text(0, 0, "Spied START vai ENTER", {
         fontFamily: "Arial",
@@ -54,7 +48,6 @@ class Intro extends Phaser.Scene {
       .setOrigin(0.5);
     this._hint = hint;
 
-    // mirgošana (tween ref, lai var kill on shutdown)
     this._hintTween = this.tweens.add({
       targets: hint,
       alpha: 0.15,
@@ -83,17 +76,16 @@ class Intro extends Phaser.Scene {
 
     const pressIn = () => {
       btnBg.setFillStyle(0x2a587c, 1);
-      this.tweens.killTweensOf([btnBg, btnText]); // stabilitāte: nav tween-spam
+      this.tweens.killTweensOf([btnBg, btnText]);
       this.tweens.add({ targets: [btnBg, btnText], scaleX: 0.96, scaleY: 0.96, duration: 70 });
     };
 
     const pressOut = () => {
       btnBg.setFillStyle(0x1f3a52, 1);
-      this.tweens.killTweensOf([btnBg, btnText]); // stabilitāte: nav tween-spam
+      this.tweens.killTweensOf([btnBg, btnText]);
       this.tweens.add({ targets: [btnBg, btnText], scaleX: 1.0, scaleY: 1.0, duration: 90 });
     };
 
-    // 5) Start (drošs pret dubult-trigger)
     const doStart = async () => {
       if (this._starting) return;
       this._starting = true;
@@ -101,7 +93,7 @@ class Intro extends Phaser.Scene {
       btnBg.disableInteractive();
       if (this.input.keyboard) this.input.keyboard.enabled = false;
 
-      // mūzika 1x uz visu spēli (pēc user gesture)
+      // bgm 1x (pēc user gesture)
       if (!this.sound.get("bgm")) {
         if (this.sound.context && this.sound.context.state === "suspended") {
           try {
@@ -124,26 +116,20 @@ class Intro extends Phaser.Scene {
     btnBg.on("pointercancel", () => pressOut());
     this.input.keyboard.once("keydown-ENTER", () => doStart());
 
-    // 6) FADE-IN no melna (kā tev bija)
     this.cameras.main.fadeIn(350, 0, 0, 0);
 
-    // ---------- Layout / Resize ----------
     const applyLayout = (W, H) => {
-      // bg cover
       bg.setPosition(W / 2, H / 2);
       const s = Math.max(W / bg.width, H / bg.height);
       bg.setScale(s);
 
-      // gradient redraw
       const gradH = 280;
       grad.clear();
       grad.fillGradientStyle(0x000000, 0x000000, 0x000000, 0x000000, 0.0, 0.0, 0.92, 0.92);
       grad.fillRect(0, H - gradH, W, gradH);
 
-      // © pozīcija
       copyright.setPosition(W - 12, 10);
 
-      // >>> START pogas izvietojums (kanoniski, kā tev patīk)
       const hintY = H - (isDesktop ? 165 : 150);
       const btnY = hintY + 75;
 
@@ -152,27 +138,20 @@ class Intro extends Phaser.Scene {
       btnText.setPosition(W / 2, btnY);
     };
 
-    // initial
     applyLayout(this.scale.width, this.scale.height);
 
-    // resize handler
-    this._onResize = (gameSize) => {
-      applyLayout(gameSize.width, gameSize.height);
-    };
+    this._onResize = (gameSize) => applyLayout(gameSize.width, gameSize.height);
     this.scale.on("resize", this._onResize);
 
-    // cleanup
     this.events.once(Phaser.Scenes.Events.SHUTDOWN, this.onShutdown, this);
     this.events.once(Phaser.Scenes.Events.DESTROY, this.onShutdown, this);
   }
 
   onShutdown() {
-    // resize off
     try {
       if (this._onResize) this.scale.off("resize", this._onResize);
     } catch (e) {}
 
-    // kill hint tween
     try {
       if (this._hintTween) this._hintTween.stop();
     } catch (e) {}
@@ -180,7 +159,6 @@ class Intro extends Phaser.Scene {
       if (this._hint) this.tweens.killTweensOf(this._hint);
     } catch (e) {}
 
-    // kill button tweens + listeners
     try {
       if (this._btnBg && this._btnText) {
         this.tweens.killTweensOf([this._btnBg, this._btnText]);
@@ -191,4 +169,4 @@ class Intro extends Phaser.Scene {
   }
 }
 
-export default Intro;
+window.Intro = Intro;
