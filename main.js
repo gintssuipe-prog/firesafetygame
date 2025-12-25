a7ad70f5fb5b834a9c87158e643f3fec78487d08
gintssuipe-prog <gints.suipe@gmail.com>
2025-12-25 03:26:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 420,
+  height: 820,
+  parent: "game",
+
+
+  dom: { createContainer: true },
+  // ✅ ŠIS IR GALVENAIS FIX portrait režīmam
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+
+  scene: [Intro, MainMenu, Stage1, Finish, Score],
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 900 },
+      debug: false
+    }
+  }
+};
+
+new Phaser.Game(config);
