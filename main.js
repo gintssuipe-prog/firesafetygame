5664f3ab87004e8c72d0674bb3b565748f113f9f
gintssuipe-prog <gints.suipe@gmail.com>
2025-12-25 03:51:29 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const config = {
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
 
-  scene: [Intro, MainMenu, Stage1, Finish, Score],
+  scene: [Intro, MainMenu, Stage1, Finish],
 
   physics: {
     default: "arcade",
